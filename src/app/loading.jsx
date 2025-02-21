2999a78005ce2c6bbeacd55585fda90d2b61488d
react-redux-project <wm635950@gmail.com>
2025-02-21 19:39:31 +0500

    add card componet

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className='flex items-center justify-center'>
+        <img src="/spinner.svg" alt="loading" />
+    </div>
+  )
+}
